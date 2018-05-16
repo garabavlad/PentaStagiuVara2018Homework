2391ddb8afdf1be6332c13fe18ba7ee9407d8353
Vladislav <garaba.vlad@gmail.com>
2018-05-16 17:02:05 +0200

    First string option.

---
+++ Module01Week02/Homework02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "My name is ";
+            string name = "Vlad";
+
+            output = string.Concat(output, name);
+            //if()
+
+        }
+    }
+}
