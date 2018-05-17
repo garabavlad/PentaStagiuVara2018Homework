38664343ce3911cf6972f23e422de854c6ab2b63
Vladislav <garaba.vlad@gmail.com>
2018-05-18 01:52:22 +0200

    Progam 2 done. Work around with 1-st one

--- Module01Week02/Homework02/Program.cs
+++ Module01Week02/Homework02/Program.cs
@@ -13,8 +13,35 @@ namespace Homework02
             string output = "My name is ";
             string name = "Vlad";
 
+            // Concatenating 2 strings.
             output = string.Concat(output, name);
-            //if()
+
+            // Other way to concat strings.
+            output += name;
+
+            // Empty string if string has elements.
+            if (output.Length != 0)
+                output = string.Empty;
+
+            // Reinit string.
+            output = "A";
+
+            // Replacing substring "A" with name substring.
+            output = output.Replace("A", name);
+
+            //Comparing strings.
+            //Teoretically they should be the same.
+            int stringIntComparator = output.CompareTo(name);
+
+            // Getting index of 'V' from the string.
+            int stringIndex = output.LastIndexOf('V');
+            stringIndex++;
+
+            // Getting next character. Here should be an if, just too lazy.
+            char charFromString = output[stringIndex];
+
+            // Getting a char array of string values.
+            char[] outputStringToCharArray = output.ToCharArray();
 
         }
     }
