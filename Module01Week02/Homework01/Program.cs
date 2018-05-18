b7280e56c553b06f078fbef70190320738941d5e
Vladislav <garaba.vlad@gmail.com>
2018-05-18 02:22:33 +0200

    Week02 | Homework01 is done!

--- Module01Week02/Homework01/Program.cs
+++ Module01Week02/Homework01/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
  *  Read from the console 3 integers representing the year, month and day of a person and a letter (M/F)
  * representing the gender of the person
  *  Convert the 3 integers and create a DateTime for the birthdate
+ *  
  *  Compute the age of the person, based on the birthdate
  *  Create an enum for the genders (Female, Male)
  *  Using the input for gender, set the value of a nullable variable to one of the enum values or to null
@@ -17,20 +18,29 @@ using System.Threading.Tasks;
  */
 namespace Homework01
 {
+
     public enum genders
     {
-        Error = -1,
-        M=0,
-        F=1
+        Male=0,
+        Female=1
     }
 
     class Program
     {
+        private const double daysPerYear = 365.25;
+        private const int retirementAgeMale = 65;
+        private const int retirementAgeFemale = 63;
+
+
+
         static void Main(string[] args)
+
         {
+
             int bornDay, bornMonth, bornYear;
-            genders personGender;
+            genders? personGender;
             
+
             Console.WriteLine("Upload data of the person:");
             Console.WriteLine("Enter the year he/she was born:");
             bornYear = Convert.ToInt32(Console.ReadLine());
@@ -41,32 +51,72 @@ namespace Homework01
             Console.WriteLine("Enter his/her gender:");
             string checkInput = Console.ReadLine();
 
+
             // Checking input for gender.
+            bool validGender = false;
             if(checkInput.Length==1)
             {
                 if(checkInput.Equals("M"))
                 {
-                    personGender = genders.M;
+                    personGender = genders.Male;
+                    validGender = true;
                 }
                 else if(checkInput.Equals("F"))
                 {
-                    personGender = genders.F;
+                    personGender = genders.Female;
+                    validGender = true;
                 }
                 else
                 {
-                    personGender = genders.Error;
+                    personGender = null;
                 }
             }
             else
             {
-                personGender = genders.Error;
+                personGender = null;
             }
 
+
+            //Getting life time span.
             DateTime personBirthdate = new DateTime(bornYear, bornMonth, bornDay);
-            TimeSpan personActualAge = DateTime.Now - personBirthdate;
-            DateTime personActualAge1 = DateTime.Now + personActualAge;
-            Console.WriteLine(personActualAge1);
-            //DateTime.
+            TimeSpan personLifeTime = DateTime.Now - personBirthdate;
+
+
+            // Getting person's age.
+            int personAge = (int)(personLifeTime.Days / daysPerYear);
+
+            // Displaying time till retirement if gender is valid.
+            if(validGender)
+            {
+                if(personGender == genders.Male)
+                {
+                    if(personAge>=retirementAgeMale)
+                    {
+                        Console.WriteLine("This male is retired.");
+                    }
+                    else
+                    {
+                        int yearTillRetirement = retirementAgeMale - personAge;
+                        Console.WriteLine("{0} more years until retirement.",yearTillRetirement);
+                    }
+                }
+                else
+                {
+                    if (personAge >= retirementAgeFemale)
+                    {
+                        Console.WriteLine("This female is retired.");
+                    }
+                    else
+                    {
+                        int yearTillRetirement = retirementAgeFemale - personAge;
+                        Console.WriteLine("{0} more years until retirement.", yearTillRetirement);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid gender input.");
+            }
 
             Console.Read();
 
